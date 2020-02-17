7ac8b197a92056be33624be928edb574511e3e90
Eptwalabha <damiengabrielle@gmail.com>
2020-02-18 00:42:35 +0100

    start implementing dialog

--- Simple.gd
+++ Simple.gd
@@ -8,28 +8,26 @@ onready var dialog_next := $UI/Dialog/Next as Button
 onready var events := $UI/Events as RichTextLabel
 onready var particle := $Particles2D as Particles2D
 onready var dialog_box := $UI/Dialog as Control
+onready var dialog_system := $DialogSystem as DialogSystem
 
-var dialog_keys : Array = []
-var dialogs := {}
+var dialogs := []
 
 func _ready() -> void:
 	select.clear()
 	var id = 0
-	for dialog in dialogs:
-		var d = dialogs[dialog]
-		dialog_keys.push_back(d.key)
-		select.add_item(d.label, id)
-		id += 1
+	for dialog in dialog_system.dialog_list():
+		select.add_item(dialog)
 	dialog_box.hide()
 
 func update_dialog(data) -> void:
-	dialog_box.visible = data.type != 'end'
-	if data.type != 'end':
+	var end_of_dialog = not data.has('text')
+	dialog_box.visible = not end_of_dialog
+	if not end_of_dialog:
 		empty_choice_container()
-		dialog_title.text = data.who
+		dialog_title.text = 'who'
 		dialog_text.set_bbcode(data.text.c_unescape())
-		dialog_next.visible = data.type == 'dialog'
-		if data.type == 'choice':
+		dialog_next.visible = true
+		if data.has('choices'):
 			for choice in data.choices:
 				add_dialog_choice(choice.text, choice.id)
 
@@ -46,17 +44,21 @@ func add_dialog_choice(label: String, choice_id: int) -> void:
 	choice_btn.connect("pressed", self, "_on_DialogChoice_pressed", [choice_id], CONNECT_ONESHOT)
 	choices_container.add_child(choice_btn)
 
-func _on_Button_pressed() -> void:
+func _on_StartDialog_pressed() -> void:
 	var selected = select.get_selected_id()
 	if selected > -1:
-		var dialog_key = dialog_keys[selected]
+		var dialog = select.get_item_text(selected)
+		dialog_system.start(dialog)
+		var data = dialog_system.next()
+		update_dialog(data)
 
 func _on_NextDialog_pressed() -> void:
-	pass
+	var data = dialog_system.next()
+	update_dialog(data)
 
 func _on_DialogChoice_pressed(choice_id: int) -> void:
 	empty_choice_container()
-	pass
+	dialog_system.choice_next(choice_id)
 
 func _on_GameGraph_event_triggered(dialog_name, event_name) -> void:
 	var error = events.append_bbcode("new event '[b][color=blue]%s[/color][/b]' from dialog [color=blue]%s[/color]\n" % [event_name, dialog_name])
--- addons/dialink/DialogSystem.gd
+++ addons/dialink/DialogSystem.gd
@@ -7,20 +7,67 @@ export(Array, String, FILE) var dialog_files := [] setget _load_new_dialog_file
 var file_valid = false
 var variables := {}
 var dialogs := {}
-var visited := []
+var visited := {}
+var current_dialog : String = ''
+var current_index : int = 0
 
-func start_dialog(key: String) -> void:
-	pass
+func start(key: String) -> void:
+	if not dialogs.has(key):
+		return
+	if not visited.has(key):
+		visited[key] = 0
+	visited[key] += 1
+	current_dialog = key
+	current_index = 0
 
 func next() -> Dictionary:
-	return {}
+	if current_dialog == '' or not dialogs.has(current_dialog):
+		return {}
+	var dialog = dialogs[current_dialog]
+	if current_index >= len(dialog):
+		return {}
+	var node = dialog[current_index]
+	if node.has('if') and _if(node.get('if')):
+		current_index += 1
+		return next()
+	
+	current_index += 1
+	if node.has('op'):
+		_op(node.get('op'))
+		return next()
+	else:
+		return _build_node(node)
+		
+
+func _build_node(node) -> Dictionary:
+	var next = {}
+	if not node.has('text'):
+		return next
+	next.text = node.text[0]
+	if node.has('choices'):
+		var choices = []
+		var id = 0
+		for choice in node['choices']:
+			id += 1
+			if choice.has('if') and not _if(choice['if']):
+				continue
+			choices.append({
+				'text': choice.text[0],
+				'id': id - 1,
+			})
+		if len(choices) > 0:
+			next['choices'] = choices
+			return next
+		else:
+			return {}
+	return next
 
 func choice_next(index: int) -> Dictionary:
 	return {}
 
-func dialogs_list() -> Array:
+func dialog_list() -> Array:
 	return dialogs.keys()
-	
+
 func variables() -> Dictionary:
 	return variables
 
@@ -57,7 +104,7 @@ func _load_new_dialog_file(files: Array) -> void:
 func _reset_node() -> void:
 	variables = {}
 	dialogs = {}
-	visited = []
+	visited = {}
 
 func _merge_dialogs(json: Dictionary) -> bool:
 	var json_dialogs = json.get("dialogs")
@@ -70,7 +117,6 @@ func _merge_dialogs(json: Dictionary) -> bool:
 	for variable in json_variables:
 		if not variables.has(variable):
 			variables[variable] = json_variables[variable]
-	print(dialogs_list())
 	return true
 
 func _op(opperation) -> void:
@@ -85,7 +131,7 @@ func _op(opperation) -> void:
 func _if(predicate) -> bool:
 	match predicate:
 		['visited', var dialog_key]:
-			return visited.has(dialog_key)
+			return visited.has(dialog_key) and visited[dialog_key] > 0
 		['not_visited', var dialog_key]:
-			return not visited.has(dialog_key)
+			return not visited.has(dialog_key) or visited[dialog_key] == 0
 	return false
