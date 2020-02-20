650d551db1741d35c5e5339307f6f9ca4faaa8f5
Eptwalabha <damiengabrielle@gmail.com>
2020-02-20 23:04:07 +0100

    add unit test + fix broken visit counter

--- addons/dialink/DialogSystem.gd
+++ addons/dialink/DialogSystem.gd
@@ -18,24 +18,46 @@ var last_node
 func start(key: String) -> void:
 	if not dialogs.has(key):
 		return
-	if not visited.has(key):
-		visited[key] = 0
-	visited[key] += 1
+	_visit(key)
 	current_dialog = key
 	parent_dialog = key
 	current_path = [0]
 
 func next() -> Dictionary:
 	var node = _current_node()
+	if node.hash() == {}.hash():
+		_clear_cursor()
+		return {}
+
 	if node.has('if'):
 		if not _if(node['if']):
 			_move_next_sibling()
 			return next()
 		else:
-			_move_forward()
+			_move_forward(node)
 			return _do_next(node)
-	_move_forward()
+	_move_forward(node)
 	return _do_next(node)
+
+func choice_next(index: int) -> Dictionary:
+	var node = _current_node()
+	if index >= node['choices'].size():
+		_clear_cursor()
+		return {}
+	current_path.push_back(index)
+	var choice_path = current_path.duplicate()
+
+	_visit(choice_path)
+
+	node = _current_node()
+	if not node.has('then'):
+		return {}
+	var then = node['then']
+	if then is String:
+		_jump_to(then)
+	else:
+		current_path.push_back(0)
+	return next()
 
 func _do_next(node) -> Dictionary:
 	_process_op(node)
@@ -53,37 +75,15 @@ func _process_then(node) -> void:
 	if node.has('then'):
 		var then = node['then']
 		if then is String:
-			jump_to(then)
+			_jump_to(then)
 		if then is Array:
 			current_path.push_back(0)
 
-func choice_next(index: int) -> Dictionary:
-	var node = _current_node()
-	if index >= len(node['choices']):
-		_clear_cursor()
-		return {}
-	current_path.push_back(index)
-	var choice_path = current_path.duplicate()
-	if not visited.has(choice_path):
-		visited[choice_path] = 0
-	visited[choice_path] += 1
-
-	node = _current_node()
-	if not node.has('then'):
-		return {}
-	var then = node['then']
-	if then is String:
-		jump_to(then)
-	else:
-		current_path.push_back(0)
-	return next()
-
-func _move_forward() -> void:
-	var node = _current_node()
+func _move_forward(node: Dictionary) -> void:
 	if node.has('then'):
 		var then = node['then']
 		if then is String:
-			jump_to(then)
+			_jump_to(then)
 		if then is Array:
 			current_path.push_back(0)
 	elif node.has('choices'):
@@ -92,11 +92,11 @@ func _move_forward() -> void:
 		_move_next_sibling()
 
 func _move_next_sibling() -> void:
-		var first = current_path.pop_back()
-		current_path.push_back(first + 1)
+	var first = current_path.pop_back()
+	current_path.push_back(first + 1)
 
 func _current_node() -> Dictionary:
-	if current_dialog == '' or !dialogs.has(current_dialog) or !len(current_path):
+	if current_dialog == '' or !dialogs.has(current_dialog) or !current_path.size():
 		return {}
 	var current = dialogs[current_dialog]
 	for index in current_path:
@@ -107,26 +107,33 @@ func _current_node() -> Dictionary:
 			element = current['choices']
 		elif current.has('then') and current['then'] is Array:
 			element = current['then']
-		
-		if element is Array and index < len(element):
+
+		if element is Array and index < element.size():
 			current = element[index]
 		else:
 			return {}
 	return current
 
-func jump_to(sub_dialog_name) -> void:
+func _jump_to(sub_dialog_name) -> void:
 	if sub_dialog_name is String:
 		var inner_jump = "%s.%s" % [parent_dialog, sub_dialog_name]
 		if dialogs.has(inner_jump):
+			_visit(inner_jump)
 			current_dialog = inner_jump
 			current_path = [0]
 		elif dialogs.has(sub_dialog_name):
+			_visit(sub_dialog_name)
 			parent_dialog = sub_dialog_name
 			current_dialog = sub_dialog_name
 			current_path = [0]
 		else:
 			_clear_cursor()
 			printerr("wtf is '%s'?" % sub_dialog_name)
+
+func _visit(key) -> void:
+	if not visited.has(key):
+		visited[key] = 0
+	visited[key] += 1
 
 func _clear_cursor() -> void:
 	parent_dialog = ''
@@ -149,7 +156,7 @@ func _build_node(node) -> Dictionary:
 			var choice_line = _build_text_node(choice.text)
 			choice_line['id'] = id - 1
 			choices.append(choice_line)
-		if len(choices) > 0:
+		if choices.size() > 0:
 			next['choices'] = choices
 			return next
 		else:
@@ -209,7 +216,7 @@ func bind_function(function_name: String, node, function) -> void:
 	pass
 
 func _get_configuration_warning() -> String:
-	if len(dialogs.keys()) == 0:
+	if dialogs.keys().size() == 0:
 		return "This node requires a valid JSON dialog's file in order to work."
 	return ""
 
--- test/unit/test_dialog_system.gd
+++ test/unit/test_dialog_system.gd
@@ -18,10 +18,10 @@ func test_dialog_list():
 		"dialog-three": [],
 	}
 	assert_true(dialog.setup_dialog(dialog_list))
-	
+
 	var expected_list1 = ['dialog-one', 'dialog-two', 'dialog-three']
 	assert_eq(expected_list1, dialog.dialog_list())
-	
+
 	for dialog_key in dialog_list:
 		dialog.start(dialog_key)
 		assert_eq(dialog_key, dialog.current_dialog)
@@ -29,8 +29,8 @@ func test_dialog_list():
 func test_dialog_next_with_text_only():
 	var dialog_list = {
 		"dialog-a": [
-			{ "text": ["text 1"] },
-			{ "text": ["text 2"] },
+			{ "text": "text 1" },
+			{ "text": "text 2" },
 			{ "text": ["text 3", " text 3.2"] },
 			{ "text": [" doctor : text 4", " text 4.2 "]},
 			{ "text": [
@@ -42,25 +42,146 @@ func test_dialog_next_with_text_only():
 				" text 7",
 				]
 			},
-			{ "text": ["x = $x"] },
-			{ "text": ["x = $x"] },
+			{ "text": "x = $x" },
+			{ "text": "x = $x" },
 		],
 	}
-	
+
 	assert_true(dialog.setup_dialog(dialog_list))
 	dialog.start('dialog-a')
-	
+
 	assert_eq("text 1", dialog.next().text)
 	assert_eq("text 2", dialog.next().text)
 	assert_eq("text 3 text 3.2", dialog.next().text)
 	var node = dialog.next()
 	assert_eq(" text 4 text 4.2 ", node.text)
 	assert_eq("doctor", node.who)
-	
+
 	assert_eq("text 5 text 7", dialog.next().text)
 	assert_eq("x = $x", dialog.next().text)
-	dialog.variables['x'] = "abc"
+	dialog.set_var("x", "abc")
+
 	assert_eq("x = abc", dialog.next().text)
-	
-#	TODO: the code segfault on the following line
-#	assert_eq({}, dialog.next())
+	assert_eq({}.hash(), dialog.next().hash())
+
+func test_dialog_with_choices():
+	var dialog_list = {
+		"choice-a": [
+			{
+				"text": "this is a choice",
+				"choices": [
+					{ "text": "choice a" },
+					{
+						"text": "choice b",
+						"then": [
+							{ "text": "sub-choice b" }
+						]
+					},
+					{
+						"text": "choice c",
+						"then": "dialog-b"
+					},
+				]
+			}
+		],
+		"dialog-b": [
+			{ "text" : "the end" }
+		]
+	}
+	dialog.setup_dialog(dialog_list)
+	dialog.start('choice-a')
+
+	var node = dialog.next()
+	assert_eq("this is a choice", node.text)
+	assert_eq(3, node.choices.size())
+	assert_eq("choice a", node.choices[0].text)
+	assert_eq(0, node.choices[0].id)
+	assert_eq("choice b", node.choices[1].text)
+	assert_eq(1, node.choices[1].id)
+	assert_eq("choice c", node.choices[2].text)
+	assert_eq(2, node.choices[2].id)
+
+	assert_eq(node.hash(), dialog.next().hash())
+
+	var next = dialog.choice_next(0)
+	assert_eq({}.hash(), next.hash())
+
+	dialog.start('choice-a')
+	dialog.next()
+	next = dialog.choice_next(1)
+	assert_eq("sub-choice b", next.text)
+
+	dialog.start('choice-a')
+	dialog.next()
+	next = dialog.choice_next(2)
+	assert_eq("the end", next.text)
+
+func test_dialog_with_conditional_choices():
+	var dialog_list = {
+		"A": [
+			{
+				"text": "alice: choice list A # super # test",
+				"choices": [
+					{
+						"if": ["not_visited", "B"],
+						"text": "visit B",
+						"then": "B"
+					},
+					{
+						"if": ["visited", "B"],
+						"text": "visit B again",
+						"then": "B"
+					},
+					{
+						"if": ["test", "foo", "eq", 2],
+						"text": "foo is 2"
+					},
+					{
+						"text": "quit"
+					}
+				]
+			}
+		],
+		"B": [
+			{
+				"text": "in dialog B",
+				"then": "A",
+			},
+		]
+	}
+	dialog.setup_dialog(dialog_list)
+	dialog.start('A')
+	var node = dialog.next()
+	assert_eq(" choice list A ", node.text)
+	assert_eq(["super", "test"], node.tags)
+	assert_eq(2, node.choices.size())
+	assert_eq("visit B", node.choices[0].text)
+	assert_eq(0, node.choices[0].id)
+	assert_eq("quit", node.choices[1].text)
+	assert_eq(3, node.choices[1].id)
+
+	node = dialog.choice_next(0)
+	assert_eq("in dialog B", node.text)
+
+	node = dialog.next()
+	assert_eq(" choice list A ", node.text)
+	assert_eq(2, node.choices.size())
+	assert_eq("visit B again", node.choices[0].text)
+	assert_eq(1, node.choices[0].id)
+	assert_eq("quit", node.choices[1].text)
+	assert_eq(3, node.choices[1].id)
+
+	dialog.set_var("foo", 2)
+	node = dialog.next()
+	assert_eq(" choice list A ", node.text)
+	assert_eq(3, node.choices.size())
+	assert_eq("visit B again", node.choices[0].text)
+	assert_eq(1, node.choices[0].id)
+	assert_eq("foo is 2", node.choices[1].text)
+	assert_eq(2, node.choices[1].id)
+	assert_eq("quit", node.choices[2].text)
+	assert_eq(3, node.choices[2].id)
+
+	node = dialog.choice_next(3)
+	assert_eq({}.hash(), node.hash())
+
